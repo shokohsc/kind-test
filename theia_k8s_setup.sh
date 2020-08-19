3ea9b4e81665a79d9eec4375441c377992493dfd
shokohsc <shokohsc@gmail.com>
2020-08-19 19:53:13 +0000

    wip

--- theia_k8s_setup.sh
+++ theia_k8s_setup.sh
@@ -3,14 +3,16 @@ docker exec -ti theia eval "$(ssh-agent -s)"
 docker exec -ti theia ssh-add ~/.ssh/id_rsa
 docker exec -ti theia cat /home/theia/.ssh/id_rsa.pub
 
-docker exec -ti theia kind create cluster
+docker exec -ti theia kind create cluster --config ./kind.yaml
+
 docker network connect kind traefik && \
 docker network connect kind theia
 export kubernetes=$(docker exec kind-external-load-balancer awk 'END{print $1}' /etc/hosts)
 docker exec -e kubernetes=$kubernertes -ti -u root theia bash -c "echo $kubernetes  kubernetes >> /etc/hosts"
-docker exec -ti theia mkdir .kube
 docker exec -ti theia bash -c "kind get kubeconfig > /home/theia/.kube/config"
 docker exec -ti theia bash -c "sed -i 's/127.0.0.1/kubernetes/g' /home/theia/.kube/config"
+
+docker exec -ti theia kubectl cluster-info --context kind-kind
 
 docker exec -ti -u root kind-control-plane sh -c "echo 'nameserver 10.96.0.10' >> /etc/resolv.conf"
 docker exec -ti -u root kind-control-plane2 sh -c "echo 'nameserver 10.96.0.10' >> /etc/resolv.conf"
