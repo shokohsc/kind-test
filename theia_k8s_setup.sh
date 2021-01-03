803b4a526b83100ac0c496f9e5d9dd940b8fa07e
shokohsc <shokohsc@gmail.com>
2021-01-03 18:31:05 +0100

    wip

--- theia_k8s_setup.sh
+++ theia_k8s_setup.sh
@@ -23,6 +23,6 @@ docker exec -ti -u root kind-control-plane3 sh -c "echo 'nameserver 10.96.0.10' 
 docker exec -ti -u root kind-control-plane apt update
 docker exec -ti -u root kind-control-plane2 apt update
 docker exec -ti -u root kind-control-plane3 apt update
-docker exec -ti -u root kind-control-plane apt install -y open-iscsi
-docker exec -ti -u root kind-control-plane2 apt install -y open-iscsi
-docker exec -ti -u root kind-control-plane3 apt install -y open-iscsi
+docker exec -ti -u root kind-control-plane apt install -y nfs-common open-iscsi
+docker exec -ti -u root kind-control-plane2 apt install -y nfs-common open-iscsi
+docker exec -ti -u root kind-control-plane3 apt install -y nfs-common open-iscsi
