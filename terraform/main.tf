1574c6b03baba4f41e7b89fd79f477e7bae145bc
shokohsc <shokohsc@gmail.com>
2021-04-03 19:12:21 +0200

    wip

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,32 @@ terraform {
       name = "kind"
     }
   }
+
+  required_version = ">= 0.13"
+
+  required_providers {
+    github = {
+      source = "integrations/github"
+      version = ">= 4.5.2"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.0.2"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.10.0"
+    }
+    flux = {
+      source  = "fluxcd/flux"
+      version = ">= 0.0.13"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
+    }
+
+  }
 }
 
 provider "kubernetes" {
@@ -18,6 +44,22 @@ provider "helm" {
   }
 }
 
+/*
 module "apps" {
     source = "./modules"
+}
+*/
+
+provider "flux" {}
+
+provider "kubectl" {}
+
+provider "github" {
+  owner = var.github_owner
+  token = var.github_token
+}
+
+# SSH
+locals {
+  known_hosts = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
 }
---
+++ terraform/variables.tf
@@ -0,0 +1,33 @@
+variable "github_owner" {
+  type        = string
+  description = "github owner"
+}
+
+variable "github_token" {
+  type        = string
+  description = "github token"
+}
+
+variable "repository_name" {
+  type        = string
+  default     = "k8s-gitops"
+  description = "github repository name"
+}
+
+variable "repository_visibility" {
+  type        = string
+  default     = "private"
+  description = "How visiable is the github repo"
+}
+
+variable "branch" {
+  type        = string
+  default     = "main"
+  description = "branch name"
+}
+
+variable "target_path" {
+  type        = string
+  default     = "clusters/kind"
+  description = "flux sync target path"
+}
